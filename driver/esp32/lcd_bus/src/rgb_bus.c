1fab9402baa08881209731075df3838217935b3b
Kevin Schlosser <kdschlosser@users.noreply.github.com>
2023-12-01 04:02:45 -0700

    adds calling back the callback function when the transfer is done.
    The RGBBus doesn't have the ability to set a callback function when transferring data to the display. I am not sure why it doesn't so if there is a callback that has been supplied that callback gets called after the function to transfer the data returns. We will see if that works out properly

--- driver/esp32/lcd_bus/src/rgb_bus.c
+++ driver/esp32/lcd_bus/src/rgb_bus.c
@@ -343,9 +343,8 @@ mp_obj_t mp_lcd_rgb_bus_make_new(const mp_obj_type_t *type, size_t n_args, size_
             mp_raise_msg_varg(&mp_type_OSError, "%d(esp_lcd_panel_draw_bitmap)", ret);
         }
 
-        if (self->callback == mp_const_none) {
-            while (self->trans_done == false) {}
-            self->trans_done = false;
+        if (self->callback != mp_const_none) {
+            mp_sched_schedule(self->callback, self->user_ctx);
         }
 
         return mp_const_none;
